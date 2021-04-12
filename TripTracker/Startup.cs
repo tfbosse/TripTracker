4f499276202e0ddc2c717d84d1cd987585b0edd2
Thomas Bosse <tfbosse97@gmail.com>
2021-04-12 10:29:20 -0400

    clean up startup files and separate client and server builds

--- TripTracker/Startup.cs
+++ TripTracker/Startup.cs
@@ -1,7 +1,5 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -63,7 +61,7 @@ namespace TripTracker
 
                 if (env.IsDevelopment())
                 {
-                    spa.UseReactDevelopmentServer(npmScript: "start");
+                    spa.UseProxyToSpaDevelopmentServer("http://localhost:3000");
                 }
             });
         }
